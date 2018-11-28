Nans fixed, weighting included, bootstrapping too. Presentation draft, latex structure

ConvNets.py
@@ -20,14 +20,14 @@ class ConvNet_3_7_15(nn.Module):
 
         self.dropout = nn.Dropout(p=0.3)
         if(input_mode=='1hot'):
-            self.conv1 = nn.Conv2d(20, 50, kernel_size_layer1, padding=pad_l1)
+            self.conv1 = nn.Conv2d(20, 25, kernel_size_layer1, padding=pad_l1)
         elif(input_mode=='protvec'):
-            self.conv1 = nn.Conv2d(100, 50, kernel_size_layer1, padding=pad_l1)
+            self.conv1 = nn.Conv2d(100, 25, kernel_size_layer1, padding=pad_l1)
         else:
             raise ValueError('wrong input mode!')
 
-        self.conv2 = nn.Conv2d(50,11,kernel_size_layer2, padding=pad_l2)
-        self.conv3 = nn.Conv2d(11,4, kernel_size_layer3, padding=pad_l3)
+        self.conv2 = nn.Conv2d(25,7,kernel_size_layer2, padding=pad_l2)
+        self.conv3 = nn.Conv2d(7,4, kernel_size_layer3, padding=pad_l3)
 
     def _get_padding( self, padding_type, kernel_size ):
         if padding_type == 'SAME': #zero padding automatically to make kernel fit
@@ -44,7 +44,7 @@ class ConvNet_1_7_15(nn.Module):
     def __init__(self, input_mode):
         super(ConvNet_1_7_15, self).__init__()
         #avg dist between beta sheets influences kernel size because kernel should capture both betasheets in one frame
-        kernel_size_layer1=(1,3)
+        kernel_size_layer1=(1,1)
         kernel_size_layer2=(1,7)
         kernel_size_layer3=(1,15)
 
@@ -57,14 +57,14 @@ class ConvNet_1_7_15(nn.Module):
 
         self.dropout = nn.Dropout(p=0.3)
         if(input_mode=='1hot'):
-            self.conv1 = nn.Conv2d(20, 50, kernel_size_layer1, padding=pad_l1)
+            self.conv1 = nn.Conv2d(20, 25, kernel_size_layer1, padding=pad_l1)
         elif(input_mode=='protvec'):
-            self.conv1 = nn.Conv2d(100, 50, kernel_size_layer1, padding=pad_l1)
+            self.conv1 = nn.Conv2d(100, 25, kernel_size_layer1, padding=pad_l1)
         else:
             raise ValueError('wrong input mode!')
 
-        self.conv2 = nn.Conv2d(50,11,kernel_size_layer2, padding=pad_l2)
-        self.conv3 = nn.Conv2d(11,4, kernel_size_layer3, padding=pad_l3)
+        self.conv2 = nn.Conv2d(25,10,kernel_size_layer2, padding=pad_l2)
+        self.conv3 = nn.Conv2d(10,3, kernel_size_layer3, padding=pad_l3)
 
     def _get_padding( self, padding_type, kernel_size ):
         if padding_type == 'SAME': #zero padding automatically to make kernel fit
@@ -74,7 +74,8 @@ class ConvNet_1_7_15(nn.Module):
     def forward(self, x):
         out=self.dropout(F.relu(self.conv1(x)))
         out = self.dropout(F.relu(self.conv2(out)))
-        out=F.log_softmax(self.conv3(out), dim=1)
+        #out=F.log_softmax(self.conv3(out), dim=1)
+        out = F.softmax(self.conv3(out), dim=1)
         return out
 
 class ConvNet_3_5_9(nn.Module):
@@ -116,11 +117,11 @@ class ConvNet_3_5_9(nn.Module):
 
 class ConvNet_1_7_3(nn.Module):
     def __init__(self, input_mode):
-        super(ConvNet_3_9_13, self).__init__()
+        super(ConvNet_1_7_3, self).__init__()
         #avg dist between beta sheets influences kernel size because kernel should capture both betasheets in one frame
-        kernel_size_layer1=(1,3)
-        kernel_size_layer2=(1,5)
-        kernel_size_layer3=(1,9)
+        kernel_size_layer1=(1,1)
+        kernel_size_layer2=(1,7)
+        kernel_size_layer3=(1,3)
 
         pad_l1 = self._get_padding('SAME', kernel_size_layer1)
         pad_l2 = self._get_padding('SAME', kernel_size_layer2)

DataLoader.py
@@ -42,9 +42,10 @@ def countStructures3(train, test):
     print('occurences test:', ctest, htest, etest, xytest, '-->', testsum)
 
 class Data_splitter():
-    def __init__(self, inputs, targets, test_ratio=0.2):  # 80% train, 20% test
+    def __init__(self, inputs, targets, masks, test_ratio=0.2):  # 80% train, 20% test
         self.inputs = inputs
         self.targets = targets
+        self.masks=masks
         self.test_ratio = test_ratio
         self.max_len = 0
 
@@ -60,10 +61,10 @@ class Data_splitter():
             if (len(self.targets[i]) > self.max_len):
                 self.max_len = len(self.targets[i])
             if (rnd > (1 - self.test_ratio)):
-                test[cnt_test] = (self.inputs[i], self.targets[i])
+                test[cnt_test] = [self.inputs[i], self.targets[i], self.masks[i]]
                 cnt_test += 1
             else:
-                train[cnt_train] = (self.inputs[i], self.targets[i])
+                train[cnt_train] = [self.inputs[i], self.targets[i], self.masks[i]]
                 cnt_train += 1
 
         print('Size of training set: {}'.format(len(train)))
@@ -75,23 +76,24 @@ class Data_splitter():
         return self.max_len
 
 class Dataset(data.Dataset):
-    # LEN_FEATURE_VEC = 1
 
-    def __init__(self, samples, max_len=None):
+    def __init__(self, samples, weights, max_len=None):
         self.max_len = max_len
-        self.inputs, self.targets = zip(*[(inputs, targets)
-                                          for _, (inputs, targets) in samples.items()])
+        self.inputs, self.targets, self.masks = zip(*[[inputs, targets, masks]
+                                          for _, [inputs, targets, masks] in samples.items()])
         self.data_len = len(self.inputs)
         self.to_tensor = transforms.ToTensor()
+        self.weights=weights
 
     def __getitem__(self, idx):
         X = self.inputs[idx]
         # print('X.shape in getitem(): ', X.shape)
         Y = self.targets[idx]
         # print('Y.shape in getitem(): ', len(Y))
-        mask_idx = np.where(np.array(Y) == 4)
-        mask = np.ones(len(X))
-        mask[mask_idx] = 0
+        mask=self.masks[idx]
+        #mask_idx = np.where(np.array(Y) == 4)
+        #mask = np.ones(len(X))
+        #mask[mask_idx] = 0
         # print('Y:', Y)
         # print('mask.shape in getitem(): ', mask.shape)
 
@@ -114,34 +116,39 @@ class Dataset(data.Dataset):
         # print('Yt_expanded.shape in getitem(): ', Y.shape)
 
         input_tensor = torch.from_numpy(X).type(dtype=torch.float)
-        # print('inputtensor:', input_tensor.size())
+        #print('inputtensor:', input_tensor.size(), input_tensor)
         output_tensor = torch.from_numpy(Y)
-        # print('outputtensor:', output_tensor.size())
+        #print('outputtensor:', output_tensor.size(), output_tensor)
         mask_tensor = torch.from_numpy(mask).type(dtype=torch.float)
-        # print('masktensor', mask_tensor.size())
-        return (input_tensor, output_tensor, mask_tensor)
+        #print('masktensor', mask_tensor.size(), mask_tensor)
+
+        weighted_mask=torch.Tensor(np.array([self.weights[i] for i in output_tensor]))*mask_tensor
+        #print('weighted:', weighted_mask)
+        assert weighted_mask.size()==mask_tensor.size()
+
+        return (input_tensor, output_tensor, weighted_mask)
 
     def __len__(self):
         return self.data_len
 
-def train_test_split(inputs, targets):
-    d = Data_splitter(inputs, targets)
+def train_test_split(inputs, targets, masks, weights):
+    d = Data_splitter(inputs, targets, masks)
     train, test = d.split_data()
     max_len = d.get_max_length()
-    train_set = Dataset(train, max_len)
-    test_set = Dataset(test, max_len)
+    train_set = Dataset(train, weights,max_len)
+    test_set = Dataset(test,weights, max_len)
 
     return train_set, test_set
 
-def createDataLoaders(train_set, test_set):
+def createDataLoaders(train_set, test_set, batch_size_train, batch_size_test):
     data_loaders = dict()
     data_loaders['Train'] = torch.utils.data.DataLoader(dataset=train_set,
-                                                        batch_size=32,
+                                                        batch_size=batch_size_train,
                                                         shuffle=True,
                                                         drop_last=False
                                                         )
     data_loaders['Test'] = torch.utils.data.DataLoader(dataset=test_set,
-                                                       batch_size=100,
+                                                       batch_size=batch_size_test,
                                                        shuffle=False,
                                                        drop_last=False
                                                        )

GetStatistics.py
@@ -67,7 +67,7 @@ def getInput(folder, classification_mode): #proteins in a folder named root+prot
             dict.update(tmp)
     return dict, lengths
 
-def countStructures3(dict):
+def countStructures3(dict, f):
     c = 0
     h = 0
     e = 0
@@ -86,10 +86,16 @@ def countStructures3(dict):
             else:
                 raise ValueError('Unknown structure', sequence[i])
 
+    f.write('\n')
+    f.write('###   DSSP3 Classes   ###\n')
+    f.write(str('(C, H, E, XY): '+str(c)+' '+str(h)+' '+str(e)+' '+str(xy)+'\n'))
+    f.write('\n')
     counts=np.array([c,h,e,xy])
     np.save(statistics_dir+'/countClasses_3',counts)
+    tmp={'proportions3':counts}
+    attributes.update(tmp)
 
-def countStructures8(dict):
+def countStructures8(dict, f):
     h = 0
     e = 0
     t = 0
@@ -123,10 +129,17 @@ def countStructures8(dict):
             else:
                 print('unknown found:', sequence[i])
                 return 0,0,0,0,0,0,0,0,0
+
+    f.write('\n')
+    f.write('###   DSSP8 Classes   ###\n')
+    f.write('(H, E, I, S, T, G, B, -, XY): '+str(h)+' '+str(e)+' '+str(i)+' '+str(s)+' '+str(t)+' '+str(g)+' '+str(b)+' '+str(none)+' '+str(xy)+'\n')
+    f.write('\n')
     counts=np.array([h,e,ii,s,t,g,b,none,xy])
     np.save(statistics_dir + '/countClasses_8', counts)
+    tmp={'proportions8':counts}
+    attributes.update(tmp)
 
-def countStructureChains3_dict(dict):
+def countStructureChains3_dict(dict, f):
     def countStructureChains3(sequence):
         C = []
         H = []
@@ -154,7 +167,7 @@ def countStructureChains3_dict(dict):
         matrix = np.zeros((max(len(np.bincount(C)), len(np.bincount(H)), len(np.bincount(E))), 3), dtype=int)
         counts = pd.DataFrame(matrix,
                               columns=['C', 'H', 'E'],
-                              index=[range(max(len(np.bincount(C)), len(np.bincount(H)), len(np.bincount(E))))])
+                              index=[np.arange(max(len(np.bincount(C)), len(np.bincount(H)), len(np.bincount(E))))])
 
         for i in range(len(np.bincount(C))):
             counts['C'][i] = np.bincount(C)[i]
@@ -163,15 +176,14 @@ def countStructureChains3_dict(dict):
         for i in range(len(np.bincount(E))):
             counts['E'][i] = np.bincount(E)[i]
 
-        # plot3combined(counts)
         return counts, C, H, E
 
     C=[]
     H=[]
     E=[]
-    counts_all = pd.DataFrame(np.zeros((0, 3), dtype=int),
+    counts_all = pd.DataFrame(np.zeros((1, 3), dtype=int),
                               columns=['C', 'H', 'E'],
-                              index=[range(0)])
+                              index=[np.arange(1)])
 
     for sample in dict.keys():
         counts_tmp, C_tmp,H_tmp,E_tmp = countStructureChains3(dict[sample][1])
@@ -181,11 +193,16 @@ def countStructureChains3_dict(dict):
         counts_all = counts_all.append(counts_tmp)
 
     counts=counts_all.groupby(counts_all.index).sum()
+    f.write('\n')
+    f.write('###   DSSP3 Chains   ###\n')
+    f.write('Average C, H, E: '+str(np.average(C))+' '+ str(np.average(H))+' '+ str(np.average(E)))
+    f.write(counts.to_string())
+    f.write('\n')
     counts.to_pickle(statistics_dir+'/countChains3')
     tmp={'avg3':[np.average(C), np.average(H), np.average(E)]}
     attributes.update(tmp)
 
-def countStructureChains8_dict(dict):
+def countStructureChains8_dict(dict, f):
     def countStructureChains8(sequence):
         H = []
         E = []
@@ -230,7 +247,7 @@ def countStructureChains8_dict(dict):
                                len(np.bincount(none))), 8), dtype=int)
         counts = pd.DataFrame(matrix,
                               columns=['H', 'E', 'I', 'S', 'T', 'G', 'B', '-'],
-                              index=[range(
+                              index=[np.arange(
                                   max(len(np.bincount(H)), len(np.bincount(E)), len(np.bincount(I)),
                                       len(np.bincount(S)), len(np.bincount(T)), len(np.bincount(G)),
                                       len(np.bincount(B)),
@@ -262,9 +279,9 @@ def countStructureChains8_dict(dict):
     B = []
     G = []
     none = []
-    counts_all = pd.DataFrame(np.zeros((0, 8), dtype=int),
+    counts_all = pd.DataFrame(np.zeros((1, 8), dtype=int),
                               columns=[ 'H', 'E', 'I', 'S', 'T', 'G', 'B', '-'],
-                              index=[range(0)])
+                              index=[np.arange(1)])
     for sample in dict.keys():
         counts_tmp, H_tmp, E_tmp, I_tmp, S_tmp, T_tmp, G_tmp, B_tmp, none_tmp = countStructureChains8(dict[sample][1])
         E.extend(E_tmp)
@@ -278,12 +295,18 @@ def countStructureChains8_dict(dict):
         counts_all = counts_all.append(counts_tmp)
 
     counts=counts_all.groupby(counts_all.index).sum()
+    f.write('\n')
+    f.write('###   DSSP8 Chains   ###\n')
+    f.write('Average H, E, I, S, T, G, B, -: '+ str(np.average(H))+' '+ str(np.average(E))+' '+ str(np.average(I))+' '+ str(np.average(S))+' '+
+                                                str(np.average(T))+' '+str(np.average(G))+' '+str(np.average(B))+' '+str(np.average(none))+'\n')
+    f.write(counts.to_string())
+    f.write('\n')
     counts.to_pickle(statistics_dir+'/countChains8')
 
     tmp={'avg8':[np.average(H), np.average(E),np.average(I),np.average(S),np.average(T),np.average(G),np.average(B),np.average(none)]}
     attributes.update(tmp)
 
-def countAminoAcidsPerStruct_dict(dict3, dict8):
+def countAminoAcidsPerStruct_dict(dict3, dict8, f):
     def countAminoAcidsPerStruct8(seq, struct):
         matrix = np.zeros((9, 21), dtype=int)
         aa_counts = pd.DataFrame(matrix,  # # 0:H, 1:E, 2:I, 3:S, 4:T, 5:G, 6:B, 7:-
@@ -384,8 +407,12 @@ def countAminoAcidsPerStruct_dict(dict3, dict8):
         aa_counts_tmp=countAminoAcidsPerStruct3(seq,struct)
         aa_counts=aa_counts.append(aa_counts_tmp)
 
-    counts = np.array([aa_counts.groupby(aa_counts.index).sum()])
-    np.save(statistics_dir + '/countAAs3', counts)
+    counts = aa_counts.groupby(aa_counts.index).sum()
+    f.write('\n')
+    f.write('###   TYPE OF AMINO ACIDS PER STRUCTURE CLASS DSSP3   ###\n')
+    f.write(counts.to_string())
+    f.write('\n')
+    np.save(statistics_dir + '/countAAs3', np.array(counts))
 
     matrix = np.zeros((9, 21), dtype=int)
     aa_counts = pd.DataFrame(matrix,  # 0:C,1:H,2:E,3:XY
@@ -401,11 +428,15 @@ def countAminoAcidsPerStruct_dict(dict3, dict8):
         aa_counts_tmp = countAminoAcidsPerStruct8(seq, struct)
         aa_counts=aa_counts.append(aa_counts_tmp)
 
-    counts=np.array([aa_counts.groupby(aa_counts.index).sum()])
-    np.save(statistics_dir+'/countAAs8',counts)
+    counts=aa_counts.groupby(aa_counts.index).sum()
+    f.write('\n')
+    f.write('###   TYPE OF AMINO ACIDS PER STRUCTURE CLASS DSSP8   ###\n')
+    f.write(counts.to_string())
+    f.write('\n')
+    np.save(statistics_dir+'/countAAs8',np.array(counts))
 
 
-def countDistanceBetweenBetaSheets_dict(dict):
+def countDistanceBetweenBetaSheets_dict(dict, f):
     def countDistanceBetweenBetaSheets(sequence):
         dist = []
         i = 0
@@ -428,17 +459,26 @@ def countDistanceBetweenBetaSheets_dict(dict):
         seq=dict[sample][1]
         dist_tmp=countDistanceBetweenBetaSheets(seq)
         dist=dist+dist_tmp
-    return np.average(dist)
 
+    f.write('\n')
+    f.write('###   AVG DISTANCE BETWEEN BETA SHEETS   ###\n')
+    f.write(str(np.average(dist)))
+    f.write('\n')
 
+f = open('statistics.txt', 'w')
 dict3, lengths3 = getInput(proteins_path, 3)
 dict8, lengths8 = getInput(proteins_path, 8)
+f.write('### LENGTHS ###\n')
+f.write('Lengths of sequences in dataset: (min, max, avg)'+str(np.min(lengths3))+' '+str(np.max(lengths3))+' '+ str(np.average(lengths3))+'\n')
 
-countStructures3(dict3)
-countStructures8(dict8)
-countStructureChains3_dict(dict3)
-countStructureChains8_dict(dict8)
+countStructures3(dict3, f)
+countStructures8(dict8, f)
+countStructureChains3_dict(dict3, f)
+countStructureChains8_dict(dict8, f)
+countAminoAcidsPerStruct_dict(dict3, dict8, f)
+countDistanceBetweenBetaSheets_dict(dict3, f)
+countDistanceBetweenBetaSheets_dict(dict8, f)
 
 np.save(statistics_dir+'/lengths3',np.array(lengths3))
-print(len(attributes))
 np.save(statistics_dir+'/stats_dict', attributes)
+f.close()

InputToMatrix.py
@@ -8,25 +8,28 @@ proteins_path='../mheinzinger/contact_prediction_v2/targets/dssp'
 seq_path='../mheinzinger/contact_prediction_v2/sequences'
 #protvec_path='../mheinzinger/deepppi1tb/contact_prediction/contact_prediction_v2/protVecs/protVec_100d_3grams.csv'
 protvec_path='../mheinzinger/contact_prediction_v2/protVecs/protVec_100d_3grams.csv'
+save_path='matrices_012'
 
-DSSP3_MAPPING = { 'C' : 1,
-                  'H' : 2,
-                  'E' : 3,
-                  'Y' : 4, # map ambigious residues also to E, mask out later
-                  'X' : 4  # map missing residues also to E, mask out later
-                  }
-
-DSSP8_MAPPING = { 'H' : 1,
+DSSP3_MAPPING = { 'C' : 0,
+                  'H' : 1,
                   'E' : 2,
-                  'I' : 3,
-                  'S' : 4,
-                  'T' : 5,
-                  'G' : 6,
-                  'B' : 7,
-                  '-' : 8,
-                  'Y' : 9, # map ambigious residues also to E, mask out later
-                  'X' : 9  # map missing residues also to E, mask out later
+                  'Y' : 2, # map ambigious residues also to E, mask out later
+                  'X' : 2  # map missing residues also to E, mask out later
                   }
+
+DSSP8_MAPPING = { 'H' : 0,
+                  'E' : 1,
+                  'I' : 2,
+                  'S' : 3,
+                  'T' : 4,
+                  'G' : 5,
+                  'B' : 6,
+                  '-' : 7,
+                  'Y' : 7, # map ambigious residues also to E, mask out later
+                  'X' : 7  # map missing residues also to E, mask out later
+                  }
+
+DSSP3_NAN = { 'X', 'Y' }
 
 def readResidues(protein):
     path=seq_path+'/'+protein.upper()+'.fasta.txt'
@@ -153,7 +156,6 @@ def countStructures3(sequence):
 PROTVECS=protVec2dict()
 
 dict_3 = getInput(proteins_path, 3)
-print(len(dict_3))
 dict_8 = getInput(proteins_path, 8)
 matrix_train=[]
 matrix_val=[]
@@ -163,6 +165,8 @@ targets_val_3=[]
 targets_val_8=[]
 targets_train_3=[]
 targets_train_8=[]
+masks_train=[]
+masks_val=[] # TODO: check if mask is same for dssp3 and dssp8!
 
 np.random.seed(42) #to reproduce splits
 val_dict={}
@@ -187,12 +191,14 @@ for tmp in dict_3.keys():
     struct_8 = dict_8[tmp][1]
     struct_memmap_3 = [DSSP3_MAPPING[dssp_state] for dssp_state in struct_3]
     struct_memmap_8 = [DSSP8_MAPPING[dssp_state] for dssp_state in struct_8]
+    mask=[ 0 if dssp_state in DSSP3_NAN else 1 for dssp_state in struct_3 ]
     tmp_c, tmp_h, tmp_e, tmp_xy = countStructures3(struct_3)
     if (rnd > 0.8):
         matrix_val.append(m)
         protvec_val.append(pv)
         targets_val_3.append(struct_memmap_3)
         targets_val_8.append(struct_memmap_8)
+        masks_val.append(mask)
         c_val+=tmp_c
         h_val+=tmp_h
         e_val+=tmp_e
@@ -202,6 +208,7 @@ for tmp in dict_3.keys():
         protvec_train.append(pv)
         targets_train_3.append(struct_memmap_3)
         targets_train_8.append(struct_memmap_8)
+        masks_train.append(mask)
         c_train += tmp_c
         h_train += tmp_h
         e_train += tmp_e
@@ -222,10 +229,13 @@ targets_val_3_np=np.array(targets_val_3)
 print('targets 3 val shape=',targets_val_3_np.shape)
 targets_val_8_np=np.array(targets_val_8)
 print('targets 8 val shape=',targets_val_8_np.shape)
-np.save('matrix_1hot_val.npy', matrix_val_np)
-np.save('matrix_protvec_val.npy', protvec_val_np)
-np.save('targets_3_val.npy',targets_val_3_np)
-np.save('targets_8_val.npy',targets_val_8_np)
+masks_val_np=np.array(masks_val)
+print('masks val shape=',masks_val_np.shape)
+np.save(save_path+'/matrix_1hot_val.npy', matrix_val_np)
+np.save(save_path+'/matrix_protvec_val.npy', protvec_val_np)
+np.save(save_path+'/targets_3_val.npy',targets_val_3_np)
+np.save(save_path+'/targets_8_val.npy',targets_val_8_np)
+np.save(save_path+'/masks_3_val.npy',masks_val_np)
 
 matrix_train_np=np.array(matrix_train)
 print('matrix train shape=',matrix_train_np.shape)
@@ -235,8 +245,11 @@ targets_train_3_np=np.array(targets_train_3)
 print ('targets train 3 shape=',targets_train_3_np.shape)
 targets_train_8_np=np.array(targets_train_8)
 print ('targets train 8 shape=',targets_train_8_np.shape)
-np.save('matrix_1hot_train.npy', matrix_train_np)
-np.save('matrix_protvec_train.npy', protvec_train_np)
-np.save('targets_3_train.npy',targets_train_3_np)
-np.save('targets_8_train.npy',targets_train_8_np)
+masks_train_np=np.array(masks_train)
+print('masks train shape=',masks_train_np.shape)
+np.save(save_path+'/matrix_1hot_train.npy', matrix_train_np)
+np.save(save_path+'/matrix_protvec_train.npy', protvec_train_np)
+np.save(save_path+'/targets_3_train.npy',targets_train_3_np)
+np.save(save_path+'/targets_8_train.npy',targets_train_8_np)
+np.save(save_path+'/masks_3_train.npy',masks_train_np)
 print('done')

PlotStatistics.py
@@ -4,94 +4,205 @@ import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 import pandas as pd
 import os
+from matplotlib import rcParams
+rcParams.update({'figure.autolayout': True})
 
 plot_path='stats'
 sns.set(style="white", palette="muted", color_codes=True)
 sns.set_style('ticks')
+sns.set_context('talk') #'paper'
+sns.despine()
 
 dict=np.load(plot_path+'/stats_dict.npy').item() #avg3, avg8
 
 classCounts3=np.load(plot_path+'/countClasses_3.npy')
 classCounts8=np.load(plot_path+'/countClasses_8.npy')
 # array [c,h,e,xy] of occurences
-print(classCounts8)
-print(classCounts3)
 
-plt.figure()
-plt.pie(classCounts3, labels=['C', 'H', 'E', 'X or Y'], autopct='%1.1f%%',colors=['gold', 'yellowgreen', 'lightcoral', 'grey'], shadow=False, startangle=90, explode=[0,0,0,0.1])
+plt.figure(figsize=(20,10))
+plt.tight_layout()
+
+#
+# PLOT PIECHARTS
+#
+
+classcounts8_ordered=[classCounts8[4], classCounts8[3],classCounts8[7],classCounts8[0],classCounts8[2],classCounts8[5],classCounts8[6],classCounts8[1], classCounts8[8]]
+
+plt.pie(classCounts3, labels=['C', 'H', 'E', 'X or Y'], autopct='%1.1f%%',textprops={'fontsize': 25, 'fontweight':'bold'},pctdistance=1.6,colors=['gold', 'yellowgreen', 'lightcoral', 'grey'], shadow=False, startangle=90, explode=[0,0,0,0.1])
 plt.savefig(plot_path+'/piechart_3classes.pdf')
 plt.clf()
-plt.pie(classCounts8, labels=['H','E','I','S','T','G','B','-','X or Y'], autopct='%1.1f%%',pctdistance=1.3,colors=['gold', 'orange', 'yellow', 'yellowgreen','palegreen', 'forestgreen','lightcoral','firebrick','grey'], shadow=False, explode=[0,0,0,0,0,0,0,0,0.1],startangle=90)
+plt.pie(classcounts8_ordered, labels=['T','S','-','H','I','G','B','E','X or Y'], autopct='%1.1f%%',textprops={'fontsize': 25, 'fontweight':'bold'},pctdistance=1.6,colors=['gold', 'orange', 'yellow', 'yellowgreen','palegreen', 'forestgreen','lightcoral','firebrick','grey'], shadow=False, explode=[0,0,0,0,0,0,0,0,0.1],startangle=90)
 plt.savefig(plot_path+'/piechart_8classes.pdf')
-
+'''
+#
+# PLOT LENGTHS
+#
 lengthCounts3=np.load(plot_path+'/lengths3.npy')
+print(lengthCounts3)
+print(np.bincount(lengthCounts3))
 
+sns.set_context('talk')
 plt.clf()
-dist3plot=sns.distplot(lengthCounts3)
+dist3plot=sns.distplot(lengthCounts3, kde=False)
+plt.title('Occurences of sequence lengths in data set')
+dist3plot.set(xlabel='Occurences')
+dist3plot.set(ylabel='sequence length')
+#dist3plot.set(xlim=[0, 1200])
+plt.axvline(np.average(lengthCounts3),linestyle='dashed')
 fig=dist3plot.get_figure()
 plt.tight_layout()
 fig.savefig(plot_path+'/lengths.pdf')
 
 
-
+#
+# PLOT CHAIN LENGTHS 3
+#
 chainCounts3=pd.read_pickle(plot_path+'/countChains3')
-chainCounts8=pd.read_pickle(plot_path+'/countChains8')
 #columns=c,h,e, rows=lengths
 
 
 plt.clf()
-f, axes = plt.subplots(1, 3, figsize=(14,14), sharex=False, sharey=True)
+fig, axes = plt.subplots(1, 3, figsize=(28,14), sharex=False, sharey=True)
 
-first=sns.barplot(np.arange(60),chainCounts3['C'][:60] , color="skyblue", ax=axes[0])
-second=sns.barplot(np.arange(60),chainCounts3['H'][:60] , color="olive", ax=axes[ 1])
-third=sns.barplot(np.arange(60),chainCounts3['E'][:60] , color="green", ax=axes[ 2])
-first.set(xticks=[10,20,30,40,50,60])
-first.set(xticklabels=[10,20,30,40,50,60])
-second.set(xticks=[10,20,30,40,50,60])
-second.set(xticklabels=[10,20,30,40,50,60])
-third.set(xticks=[10,20,30,40,50,60])
-third.set(xticklabels=[10,20,30,40,50,60])
+first=sns.barplot(np.arange(50),chainCounts3['C'][:50] , color="skyblue", ax=axes[0])
+second=sns.barplot(np.arange(50),chainCounts3['H'][:50] , color="olive", ax=axes[ 1])
+third=sns.barplot(np.arange(50),chainCounts3['E'][:50] , color="green", ax=axes[ 2])
+first.set(xticks=[10,20,30,40,50])
+first.set(xticklabels=[10,20,30,40,50])
+second.set(xticks=[10,20,30,40,50])
+second.set(xticklabels=[10,20,30,40,50])
+third.set(xticks=[10,20,30,40,50])
+third.set(xticklabels=[10,20,30,40,50])
 
-first.set(xlabel='Chain length C')
-second.set(xlabel='Chain length H')
-third.set(xlabel='Chain length E')
+first.set(xlabel='C')
+second.set(xlabel='H')
+third.set(xlabel='E')
 first.set(ylabel='Occurences')
 second.set(ylabel=' ')
 third.set(ylabel=' ')
 
+
 plt.savefig(plot_path+'/chains3.pdf')
 
+
+
+#
+# PLOT CHAIN LENGTHS 8
+#
+chainCounts8=pd.read_pickle(plot_path+'/countChains8')
+print(chainCounts8.max())
 plt.clf()
-plot=sns.barplot(np.arange(len(chainCounts8['H'])),chainCounts8['H'], color="skyblue")
+first=sns.barplot(np.arange(50),chainCounts8['H'][:50], color="skyblue")
+first.set(xticks=[10,20,30,40,50])
+first.set(xticklabels=[10,20,30,40,50])
 plt.savefig(plot_path+'/chains8_H.pdf')
 
 plt.clf()
-plot=sns.barplot(np.arange(len(chainCounts8['E'])),chainCounts8['E'], color="olive")
+second=sns.barplot(np.arange(50),chainCounts8['E'][:50], color="olive")
+second.set(xticks=[10,20,30,40,50])
+second.set(xticklabels=[10,20,30,40,50])
 plt.savefig(plot_path+'/chains8_E.pdf')
 
 plt.clf()
-plot=sns.barplot(np.arange(len(chainCounts8['I'])),chainCounts8['I'], color="skyblue")
+third=sns.barplot(np.arange(50),chainCounts8['I'][:50], color="skyblue")
+third.set(xticks=[10,20,30,40,50])
+third.set(xticklabels=[10,20,30,40,50])
 plt.savefig(plot_path+'/chains8_I.pdf')
 
 plt.clf()
-plot=sns.barplot(np.arange(len(chainCounts8['S'])),chainCounts8['S'], color="skyblue")
+fourth=sns.barplot(np.arange(50),chainCounts8['S'][:50], color="skyblue")
+fourth.set(xticks=[10,20,30,40,50])
+fourth.set(xticklabels=[10,20,30,40,50])
 plt.savefig(plot_path+'/chains8_S.pdf')
 
 plt.clf()
-plot=sns.barplot(np.arange(len(chainCounts8['T'])),chainCounts8['T'], color="skyblue")
+fifth=sns.barplot(np.arange(50),chainCounts8['T'][:50], color="skyblue")
+fifth.set(xticks=[10,20,30,40,50])
+fifth.set(xticklabels=[10,20,30,40,50])
 plt.savefig(plot_path+'/chains8_T.pdf')
 
 plt.clf()
-plot=sns.barplot(np.arange(len(chainCounts8['G'])),chainCounts8['G'], color="skyblue")
+sixth=sns.barplot(np.arange(50),chainCounts8['G'][:50], color="skyblue")
+sixth.set(xticks=[10,20,30,40,50])
+sixth.set(xticklabels=[10,20,30,40,50])
 plt.savefig(plot_path+'/chains8_G.pdf')
 
 plt.clf()
-plot=sns.barplot(np.arange(len(chainCounts8['B'])),chainCounts8['B'], color="skyblue")
+seventh=sns.barplot(np.arange(50),chainCounts8['B'][:50], color="skyblue")
+seventh.set(xticks=[10,20,30,40,50])
+seventh.set(xticklabels=[10,20,30,40,50])
 plt.savefig(plot_path+'/chains8_B.pdf')
 
 plt.clf()
-plot=sns.barplot(np.arange(len(chainCounts8['-'])),chainCounts8['-'], color="skyblue")
+eightth=sns.barplot(np.arange(50),chainCounts8['-'][:50], color="skyblue")
+eightth.set(xticks=[10,20,30,40,50])
+eightth.set(xticklabels=[10,20,30,40,50])
 plt.savefig(plot_path+'/chains8_none.pdf')
+
+
+plt.savefig(plot_path+'/chains8.pdf')
+
+def together():
+    chainCounts8=pd.read_pickle(plot_path+'/countChains8')
+    print(chainCounts8.max())
+    plt.clf()
+    fig, axes = plt.subplots(2, 4, figsize=(28,14), sharex=True, sharey=True)
+    first=sns.barplot(np.arange(50),chainCounts8['H'][:50], color="skyblue", ax=axes[0,0])
+    #plt.savefig(plot_path+'/chains8_H.pdf')
+
+    #plt.clf()
+    second=sns.barplot(np.arange(50),chainCounts8['E'][:50], color="olive", ax=axes[0,1])
+    #plt.savefig(plot_path+'/chains8_E.pdf')
+
+    #plt.clf()
+    third=sns.barplot(np.arange(50),chainCounts8['I'][:50], color="skyblue", ax=axes[0,2])
+    #plt.savefig(plot_path+'/chains8_I.pdf')
+
+    #plt.clf()
+    fourth=sns.barplot(np.arange(50),chainCounts8['S'][:50], color="skyblue", ax=axes[0,3])
+    #plt.savefig(plot_path+'/chains8_S.pdf')
+
+    #plt.clf()
+    fifth=sns.barplot(np.arange(50),chainCounts8['T'][:50], color="skyblue", ax=axes[1,0])
+    #plt.savefig(plot_path+'/chains8_T.pdf')
+
+    #plt.clf()
+    sixth=sns.barplot(np.arange(50),chainCounts8['G'][:50], color="skyblue", ax=axes[1,1])
+    #plt.savefig(plot_path+'/chains8_G.pdf')
+
+    #plt.clf()
+    seventh=sns.barplot(np.arange(50),chainCounts8['B'][:50], color="skyblue", ax=axes[1,2])
+    #plt.savefig(plot_path+'/chains8_B.pdf')
+
+    #plt.clf()
+    eightth=sns.barplot(np.arange(50),chainCounts8['-'][:50], color="skyblue", ax=axes[1,3])
+    #plt.savefig(plot_path+'/chains8_none.pdf')
+    fifth.set(xticks=[10,20,30,40,50])
+    sixth.set(xticks=[10,20,30,40,50])
+    seventh.set(xticks=[10,20,30,40,50])
+    eightth.set(xticks=[10,20,30,40,50])
+
+    first.set(xlabel='H')
+    second.set(xlabel='E')
+    third.set(xlabel='I')
+    fourth.set(xlabel='S')
+    fifth.set(xlabel='T')
+    sixth.set(xlabel='G')
+    seventh.set(xlabel='B')
+    eightth.set(xlabel='-')
+    first.set(ylabel='Occurences')
+    fifth.set(ylabel='Occurences')
+    second.set(ylabel=' ')
+    third.set(ylabel=' ')
+    fourth.set(ylabel=' ')
+    sixth.set(ylabel=' ')
+    seventh.set(ylabel=' ')
+    eightth.set(ylabel=' ')
+
+    fig.suptitle('Chain lengths of DSSP8 classes')
+
+    plt.savefig(plot_path+'/chains8.pdf')
 
 #countAAs3=np.load(plot_path+'/countAAs3.npy')
 #countAAs8=np.load(plot_path+'/countAAs8.npy')
+'''

StructurePredictionPipeline.py
@@ -5,101 +5,147 @@ import torch.nn.functional as F
 import torch.utils.data as data
 import torchvision.transforms as transforms
 import  utilities_MICHAEL as utilities
+from sklearn.metrics import accuracy_score
 
 import ConvNets
 import DataLoader
 
-
 def trainNet(model, opt, crit, train_loader):
     model.train()
+    total_loss_train = 0
+    Y_true_all = np.zeros(0, dtype=np.int)
+    Y_pred_all = np.zeros(0, dtype=np.int)
+    c=0
+    w=0
     for i, (X, Y_true, mask) in enumerate(train_loader):
-
         X = X.to(device)
-        #print('train x size:',X.size())
         Y_true = Y_true.to(device)
-        #print('ytrue train size: ',Y_true[0])
         mask=mask.to(device)
 
         opt.zero_grad()
-        #print('X:',X)
         Y_raw=model(X)
-        #print('train yraw size:',Y_raw[0])
         loss=crit(Y_raw, Y_true)
         loss*=mask
-        loss = loss.sum() /mask.sum()
-        #print('training avg loss=',loss)
+        loss = loss.sum() / float(mask.sum())
+        total_loss_train += loss.item()
+        Y_pred_unmasked = torch.argmax(Y_raw.data,dim=1)  # returns index of output predicted with highest probability
+        #Y_pred = Y_pred_unmasked[mask == 1]
+        #Y_true = Y_true[mask == 1]
+        Y_pred = Y_pred_unmasked[mask != 0]
+        Y_true = Y_true[mask != 0]
+
+        '''
+        for i in range(mask.size()[2]):
+            if (mask[0][0][i] == 1):
+                if (Y_pred[i] == Y_true[i]):
+                    c += 1
+                else:
+                    w += 1
+        '''
+
+        Y_true = Y_true.view(-1).long().cpu().numpy()
+        Y_pred = Y_pred.view(-1).long().cpu().numpy()
+
+        Y_true_all = np.append(Y_true_all, Y_true)
+        Y_pred_all = np.append(Y_pred_all, Y_pred)
+
         loss.backward()
         opt.step()
+    #accuracy = (c / float(c + w)) * 100
+    total_loss_train=total_loss_train / len(train_loader)
+    print('--', len(train_loader),total_loss_train)
+    return total_loss_train, Y_true_all, Y_pred_all
 
-def testNet(model, data_loaders, crit, epoch, eval_summary):
+def testNet(model, test_loader, crit, epoch, eval_summary, final_flag):
     model.eval()
     with torch.no_grad():
+        total_loss_test=0
+        bootstrapping=-1
+        Y_true_all = np.zeros(0, dtype=np.int)
+        Y_pred_all = np.zeros(0, dtype=np.int)
+        confusion_matrix=np.zeros((3,3), dtype=np.int)
 
-        running_loss = ''
-        for test_set_name, test_loader in data_loaders.items():  # for Train & Test
+        bootstrapped_conf_mat=[]
 
-            loss_avg = torch.zeros(1, dtype=torch.float, device=device, requires_grad=False)
-            Y_true_all = np.zeros(0, dtype=np.int)
-            Y_pred_all = np.zeros(0, dtype=np.int)
-            confusion_matrix=np.zeros((5,5), dtype=np.int)
+        c = 0
+        w = 0
 
-            percentage_correct=[]
+        for X, Y, mask in test_loader: #batchsize samples in X and Y
+            X = X.to(device)
+            Y_true_unmasked = Y.to(device)
+            mask = mask.to(device)
+            Y_computed = model(X)
+            loss=crit(Y_computed, Y_true_unmasked)
+            loss*=mask
+            loss = (loss.sum()) / float(mask.sum())
+            total_loss_test+=loss.item()
+            Y_pred_unmasked = torch.argmax(Y_computed.data, dim=1)  # returns index of output predicted with highest probability
+            #Y_pred = Y_pred_unmasked[ mask==1 ]
+            #Y_true = Y_true_unmasked[ mask==1 ]
+            Y_pred = Y_pred_unmasked[mask != 0]
+            Y_true = Y_true_unmasked[mask != 0]
 
-            for X, Y, mask in test_loader:
-                X=X.to(device)
-                Y_true=Y.to(device)
-                mask=mask.to(device)
-                #print('test input: ', X)
-                Y_computed = model(X)
-                #print('y-raw.shape:', Y_computed.size())
-                #print('adding to loss avg: ',((crit(Y_computed, Y_true)*mask).sum()) / mask.sum())
-                loss_avg += ((crit(Y_computed, Y_true)*mask).sum()) / mask.sum()
-                Y_pred = torch.argmax(Y_computed.data, dim=1)  # returns index of output predicted with highest probability
+            '''
+            for i in range(mask.size()[2]):
+                if(mask[0][0][i]==1):
+                    if (Y_pred[i] == Y_true[i]):
+                        c += 1
+                    else:
+                        w += 1
+            '''
+            if (final_flag):
+                for i in range(Y_true_unmasked.size()[0]):
+                    mask_sample=mask[i]
+                    Y_true_sample=Y_true_unmasked[i]
+                    Y_pred_sample=Y_pred_unmasked[i]
+                    Y_true_sample=Y_true_sample[mask_sample!=0]
+                    Y_pred_sample=Y_pred_sample[mask_sample!=0]
+                    #print(mask_sample.size(), Y_true_sample.size(), Y_pred_sample.size())
+                    confusion_matrix_per_sample = np.zeros((3, 3), dtype=np.int)
+                    np.add.at(confusion_matrix_per_sample, (Y_true_sample, Y_pred_sample), 1)
+                    print(confusion_matrix_per_sample)
+                    bootstrapped_conf_mat.insert(0, confusion_matrix_per_sample)
 
-                #print('true structures: ', Y_true)
-                #print('predicted structures: ', Y_pred)
-                #print('mask: ', mask.size())
-                c = 0
-                w = 0
-                for i in range(mask.size()[2]):
-                    if(mask[0][0][i]==1):
-                        if (Y_pred[0][0][i] == Y_true[0][0][i]):
-                            c += 1
-                        else:
-                            w += 1
+            #percentage_correct.insert(0,c / float(c + w))
+            Y_true = Y_true.view(-1).long().cpu().numpy()
+            Y_pred = Y_pred.view(-1).long().cpu().numpy()
+            np.add.at(confusion_matrix, (Y_true, Y_pred), 1)
 
-                percentage_correct.insert(0,c / float(c + w))
+            Y_true_all = np.append(Y_true_all, Y_true)
+            Y_pred_all = np.append(Y_pred_all, Y_pred)
 
-                Y_true = Y_true.view(-1).long().cpu().numpy()
-                Y_pred = Y_pred.view(-1).long().cpu().numpy()
-                #print('Y_true:', Y_true)
-                #print('Y_pred:',Y_pred)
-                np.add.at(confusion_matrix, (Y_true, Y_pred), 1)
 
-                Y_true_all = np.append(Y_true_all, Y_true)
-                Y_pred_all = np.append(Y_pred_all, Y_pred)
 
-            loss_avg = (loss_avg / len(test_loader)).cpu().numpy()[0]
-            running_loss += '{0}: {1:.3f} '.format(test_set_name, loss_avg)
+        total_loss_test = (total_loss_test / len(test_loader))
 
-            utilities.add_progress(eval_summary, test_set_name, loss_avg, epoch,
-                           confusion_matrix[1:4,1:4], Y_true_all, Y_pred_all)
+        if(final_flag):
+            #print('# BOOTSTRAPPING SAMPLES: ', len(bootstrapped_conf_mat))
+            #utilities.add_progress(eval_summary, 'Test', total_loss, epoch,
+            #                       confusion_matrix, Y_true_all, Y_pred_all, bootstrapped_conf_mat)
+            #print(bootstrapped_conf_mat)
+            utilities.plot_bootstrapping(LOG_PATH, bootstrapped_conf_mat)
+        #else:
+        #utilities.add_progress(eval_summary, 'Test', total_loss, epoch,
+        #               confusion_matrix, Y_true_all, Y_pred_all)
 
-    print('running loss: ',running_loss)
-    print('avg correct percentage:', np.average(percentage_correct))
-    print('confusion mat:', confusion_matrix)
+
+    #accuracy=(c/float(c+w))*100
+    return total_loss_test, Y_true_all, Y_pred_all, confusion_matrix
 
 
 INPUT_MODE='1hot'  #protvec or 1hot
 DSSP_MODE=3        #3 or 8
-LOG_PATH='log/CNN_'+INPUT_MODE+'_'+str(DSSP_MODE)+'_1-7-15_relu'
+LOG_PATH='log/CNN_'+INPUT_MODE+'_'+str(DSSP_MODE)+'_1_7_15_relu'
+STATS_PATH='stats'
+MATRIX_PATH='matrices_012'
+stats_dict=np.load(STATS_PATH+'/stats_dict.npy').item()
 
 NUM_EPOCHS=100
-LEARNING_RATE=1e-3
+LEARNING_RATE= 1e-3
 
-
-inputs = np.load('matrix_'+INPUT_MODE+'_train.npy', fix_imports=True)
-targets=np.load('targets_'+str(DSSP_MODE)+'_train.npy', fix_imports=True)
+inputs = np.load(MATRIX_PATH+'/matrix_'+INPUT_MODE+'_train.npy', fix_imports=True)
+targets=np.load(MATRIX_PATH+'/targets_'+str(DSSP_MODE)+'_train.npy', fix_imports=True)
+masks=np.load(MATRIX_PATH+'/masks_'+str(DSSP_MODE)+'_train.npy', fix_imports=True)
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print('DEVICE: ',device)
 print('INPUT MODE: ', INPUT_MODE)
@@ -108,25 +154,72 @@ print('SAMPLES:', inputs.shape,' OF SIZE ',inputs[0].shape)
 torch.manual_seed(42)
 utilities.create_logdir( LOG_PATH )
 
+for i in range(len(inputs)):
+    assert not np.any(np.isnan(inputs[i]))
+    assert not np.any(np.isnan(targets[i]))
 
-train_set, test_set=DataLoader.train_test_split(inputs, targets)
-data_loaders=DataLoader.createDataLoaders(train_set, test_set)
+weights=stats_dict['proportions3']
+print(weights)
+print(np.sum(weights))
+WEIGHTS=weights/float(np.sum(weights))
+print('WEIGHTS: ',WEIGHTS)
+
+train_set, test_set=DataLoader.train_test_split(inputs, targets, masks, WEIGHTS)
+data_loaders=DataLoader.createDataLoaders(train_set, test_set, 32, 100)
 
 model=ConvNets.ConvNet_1_7_15(INPUT_MODE).to(device)
-criterion=nn.NLLLoss(reduce=False) #reduce=False with mask
+
+
+
+#criterion=nn.NLLLoss(reduce=False) #reduce=False with mask
+criterion=nn.CrossEntropyLoss(reduce=False)
 opt=torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, amsgrad=True)
 
-
 eval_summary = dict()
+final_flag=False
+
+train_loss=[]
+test_loss=[]
+train_acc=[]
+test_acc=[]
+
 for epoch in range(NUM_EPOCHS):
+    if(epoch==NUM_EPOCHS-1):
+        final_flag=True
     print('\n')
     print('Epoch '+str(epoch))
-    trainNet(model, opt, criterion, data_loaders['Train'])
-    testNet(model, data_loaders, criterion, epoch, eval_summary)
-    utilities.save_performance(LOG_PATH, eval_summary)
-    utilities.plot_learning(LOG_PATH, eval_summary)
+    train_loss_epoch, train_true_all, train_pred_all=trainNet(model, opt, criterion, data_loaders['Train'])
+    test_loss_epoch, test_true_all,test_pred_all, confusion_matrix_epoch=testNet(model, data_loaders['Test'], criterion, epoch, eval_summary, final_flag)
+    print('TRAIN LOSS:', train_loss_epoch)
+    print('TEST LOSS:',test_loss_epoch)
+    train_loss.append(train_loss_epoch)
+    test_loss.append(test_loss_epoch)
+    print(train_pred_all, train_true_all)
+    print('acc score:',accuracy_score( train_true_all, train_pred_all ),accuracy_score( test_true_all, test_pred_all ))
+    train_acc.append(accuracy_score( train_true_all, train_pred_all ))
+    test_acc.append(accuracy_score( test_true_all, test_pred_all ))
+    #utilities.save_performance(LOG_PATH, eval_summary)
+    #utilities.plot_learning(LOG_PATH, eval_summary)
 
-utilities.plot_learning( LOG_PATH, eval_summary )
-utilities.save_performance( LOG_PATH, eval_summary, final_flag=True )
+    '''
+    if (epoch % 10) == 0: #superconvergence
+        batch_size=batch_size+8
+        data_loaders = DataLoader.createDataLoaders(train_set, test_set, batch_size)
+    '''
+
+
+#utilities.plot_learning( LOG_PATH, eval_summary)
+#utilities.save_performance( LOG_PATH, eval_summary, final_flag=True )
+
+utilities.plot_loss(LOG_PATH, train_loss, test_loss, NUM_EPOCHS)
+utilities.plot_accuracy(LOG_PATH,train_acc, test_acc, NUM_EPOCHS)
+utilities.plot_confmat(LOG_PATH, confusion_matrix_epoch )
+utilities.get_other_scores(confusion_matrix_epoch)
 
 torch.save(model.state_dict(), LOG_PATH+'/model.ckpt')
+
+
+
+
+
+